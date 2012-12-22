9b03f9aa45926a4970d6c128cdb99cc3bad0587c
inimod <c.domi@laposte.net>
2012-12-22 01:22:17 +0100

    Premère maquete de page d'index

--- home/index.php
+++ home/index.php
@@ -1,3 +1,21 @@
-<?php
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
 
-?>
+<style type="text/css">
+@import url('../styles/home.css');
+</style>
+</head>
+<body>
+<div id="tete">
+Tête vide
+</div>
+<div id="corps">
+Ceci est le corps de texte qu'il faudra soigner...
+
+</div>
+<div id="pieds">
+<a>Pieds nus</a> - <a>Validation w3c</a>
+</div>
+</body>
+</html>
